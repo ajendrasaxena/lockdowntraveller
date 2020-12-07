3b67af91ef4b10da29869898e1e7e36b14bfd53e
ajendrasaxena <ajendrasaxena24@gmail.com>
2020-12-07 21:31:05 +0530

    final commit

---
+++ lockdown_traveller/src/lockdown_traveller/Availability.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lockdown_traveller;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author ajendrasaxena
+ */
+public class Availability {
+    public int chaircarget(String a,String b,String c,String d,String e){
+        Vector<String> v=new Vector<String>();
+        Vector<String> v1=new Vector<String>();
+        
+        return 0;
+    }
+}
